53e6d3eff5a6033d8ccf9b30366b9d7fd19a9697
Raj Patil <rajp152k@gmail.com>
2023-12-03 05:54:15 +0530

    deleted comments in lua/init.lua

--- lua/init.lua
+++ lua/init.lua
@@ -5,7 +5,6 @@ local cmp = require 'cmp'
 
 cmp.setup({
 	snippet = {
-		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
 			vim.fn["vsnip#anonymous"](args.body)
 		end,
@@ -58,22 +57,8 @@ cmp.setup.cmdline(':', {
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
 	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
--- lsp_zero.format_on_save({
---   format_opts = {
---     async = false,
---     timeout_ms = 10000,
---   },
---   servers = {
---     ['pylsp'] = { 'python' },
---     ['vimls'] = { 'vimscript' },
---     ['lua_ls'] = { 'lua' },
---     ['marksman'] = { 'markdown' },
---   }
--- })
 lsp_zero.set_sign_icons({
 	error = '✘',
 	warn = '▲',
@@ -133,40 +118,25 @@ highlight_last = "IronLastSent",
 local neogit = require("neogit")
 
 neogit.setup {
-  -- Hides the hints at the top of the status buffer
   disable_hint = false,
-  -- Disables changing the buffer highlights based on where the cursor is.
   disable_context_highlighting = false,
-  -- Disables signs for sections/items/hunks
   disable_signs = false,
-  -- Do not ask to confirm the commit - just do it when the buffer is closed.
   disable_commit_confirmation = false,
-  -- Changes what mode the Commit Editor starts in. `true` will leave nvim in normal mode, `false` will change nvim to
-  -- insert mode, and `"auto"` will change nvim to insert mode IF the commit message is empty, otherwise leaving it in
-  -- normal mode.
   disable_insert_on_commit = true,
-  -- When enabled, will watch the `.git/` directory for changes and refresh the status buffer in response to filesystem
-  -- events.
   filewatcher = {
     interval = 1000,
     enabled = true,
   },
-  -- Used to generate URL's for branch popup action "pull request".
   git_services = {
     ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
     ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
     ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
   },
-  -- Allows a different telescope sorter. Defaults to 'fuzzy_with_index_bias'. The example below will use the native fzf
-  -- sorter instead. By default, this function returns `nil`.
   telescope_sorter = function()
     return require("telescope").extensions.fzf.native_fzf_sorter()
   end,
-  -- Persist the values of switches/options within and across sessions
   remember_settings = true,
-  -- Scope persisted settings on a per-project basis
   use_per_project_settings = true,
-  -- Table of settings to never persist. Uses format "Filetype--cli-value"
   ignored_settings = {
     "NeogitPushPopup--force-with-lease",
     "NeogitPushPopup--force",
@@ -174,23 +144,12 @@ neogit.setup {
     "NeogitCommitPopup--allow-empty",
     "NeogitRevertPopup--no-edit",
   },
-  -- Set to false if you want to be responsible for creating _ALL_ keymappings
   use_default_keymaps = true,
-  -- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
-  -- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
   auto_refresh = true,
-  -- Value used for `--sort` option for `git branch` command
-  -- By default, branches will be sorted by commit date descending
-  -- Flag description: https://git-scm.com/docs/git-branch#Documentation/git-branch.txt---sortltkeygt
-  -- Sorting keys: https://git-scm.com/docs/git-for-each-ref#_options
   sort_branches = "-committerdate",
-  -- Change the default way of opening neogit
   kind = "tab",
-  -- Disable line numbers and relative line numbers
   disable_line_numbers = true,
-  -- The time after which an output console is shown for slow running commands
   console_timeout = 2000,
-  -- Automatically show console if a command takes more than console_timeout milliseconds
   auto_show_console = true,
   status = {
     recent_commit_count = 10,
@@ -203,7 +162,7 @@ neogit.setup {
   },
   commit_view = {
     kind = "vsplit",
-    verify_commit = os.execute("which gpg") == 0, -- Can be set to true or false, otherwise we try to find the binary
+    verify_commit = os.execute("which gpg") == 0,
   },
   log_view = {
     kind = "tab",
@@ -227,29 +186,16 @@ neogit.setup {
     kind = "split",
   },
   signs = {
-    -- { CLOSED, OPENED }
     hunk = { "", "" },
     item = { ">", "v" },
     section = { ">", "v" },
   },
-  -- Each Integration is auto-detected through plugin presence, however, it can be disabled by setting to `false`
   integrations = {
-    -- If enabled, use telescope for menu selection rather than vim.ui.select.
-    -- Allows multi-select and some things that vim.ui.select doesn't.
     telescope = nil,
-    -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
-    -- The diffview integration enables the diff popup.
-    --
-    -- Requires you to have `sindrets/diffview.nvim` installed.
     diffview = nil,
-
-    -- If enabled, uses fzf-lua for menu selection. If the telescope integration
-    -- is also selected then telescope is used instead
-    -- Requires you to have `ibhagwan/fzf-lua` installed.
     fzf_lua = nil,
   },
   sections = {
-    -- Reverting/Cherry Picking
     sequencer = {
       folded = false,
       hidden = false,
@@ -308,7 +254,6 @@ neogit.setup {
       ["<s-tab>"] = "MultiselectTogglePrevious",
       ["<c-j>"] = "NOP",
     },
-    -- Setting any of these to `false` will disable the mapping.
     popup = {
       ["?"] = "HelpPopup",
       ["A"] = "CherryPickPopup",
